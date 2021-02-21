bdf1bf69b7c283212d6323a5391507efd279cce1
Rohin Patel <rohin.patel@ing.com>
2021-02-21 12:51:00 +0100

    Update dependent data type with dependent functions examples

--- src/main/scala/tdd/chapter4/DependentDataType.scala
+++ src/main/scala/tdd/chapter4/DependentDataType.scala
@@ -16,10 +16,19 @@ enum PowerSource :
     case Petrol
     case Pedal
 
-enum Vehicle[T]:
-    case Bicycle extends Vehicle[PowerSource.Pedal.type]
-    case Car(fuel:Int) extends Vehicle[PowerSource.Petrol.type]
-    case Bus(fuel:Int) extends Vehicle[PowerSource.Petrol.type]
+type Pedal = PowerSource.Pedal.type
+type Petrol = PowerSource.Petrol.type
+
+//Dependent type data type
+enum Vehicle[T <: PowerSource]:
+    case Bicycle extends Vehicle[Pedal]
+    case Car(fuel:Int) extends Vehicle[Petrol]
+    case Bus(fuel:Int) extends Vehicle[Petrol]
+
+trait VehicleType {
+    type P <: PowerSource
+    val resultValue:Vehicle[P]
+}
 
 /**
  * wheels : Vehicle power -> Nat
@@ -27,7 +36,17 @@ enum Vehicle[T]:
  * wheels (Car fuel) = 4
  * wheels (Bus fuel) = 4
  */
-def wheels(v:Vehicle[PowerSource]): Int = v match 
+//v1 use dependent function type
+type Wheel_DTF = (vt:VehicleType) => Int
+
+def wheels_v1:Wheel_DTF = vt => vt.resultValue match {
+    case Vehicle.Bicycle => 2
+    case Vehicle.Car(_) => 4
+    case Vehicle.Bus(_) => 4
+}
+
+//v2 directly using dependent data type
+def wheels_v2(v:Vehicle[PowerSource]): Int = v match 
     case Vehicle.Bicycle => 2
     case Vehicle.Car(_) => 4
     case Vehicle.Bus(_) => 4
@@ -37,7 +56,16 @@ def wheels(v:Vehicle[PowerSource]): Int = v match
  * refuel (Car fuel) = Car 100
  * refuel (Bus fuel) = Bus 200
  */
-def refuel: Vehicle[PowerSource.Petrol.type] => Vehicle[PowerSource.Petrol.type] = {
+//v1 use dependent function type
+type Refuel_DTF = (vt:VehicleType{type P = Petrol}) => Vehicle[vt.P]
+
+def refuel_v1:Refuel_DTF = vt => vt.resultValue match {
+    case  Vehicle.Car(_) => Vehicle.Car(100)
+    case Vehicle.Bus(_) => Vehicle.Bus(200)
+}
+
+//v2 directly using dependent data type
+def refuel_v2: Vehicle[Petrol] => Vehicle[Petrol] = {
     case  Vehicle.Car(_) => Vehicle.Car(100)
     case Vehicle.Bus(_) => Vehicle.Bus(200)
 }
@@ -50,7 +78,12 @@ def refuel: Vehicle[PowerSource.Petrol.type] => Vehicle[PowerSource.Petrol.type]
  * time. The power source is an index of the Vehicle family. The index tells
  * you exactly which Vehicle type you mean
  */
+import util.chaining.scalaUtilChainingOps
 
 @main def dependentDataType = 
-    //refuel(Vehicle.Bicycle) compilation error
-    refuel(Vehicle.Car(1))
+    //compilation errore
+    //refuel_v1(new VehicleType{type P = Pedal; val resultValue = Vehicle.Bicycle})
+    //refuel_v2(Vehicle.Bicycle) 
+    //correct calls
+    refuel_v1(new VehicleType{type P = Petrol; val resultValue = Vehicle.Car(2)}).pipe(println)
+    refuel_v2(Vehicle.Car(2)).pipe(println)
